e067d3bf5e525ea657b529446fc832f766a32a56
Marshall Paiva <marshallpaiva@hotmail.com>
2019-01-10 14:54:35 -0300

    Cricando repository e service para categoria

---
+++ src/main/java/com/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
