7e6d7d4450ccd9df3aa78882fa2edca695fe54c0
Marshall Paiva <marshallpaiva@hotmail.com>
2019-01-11 14:57:02 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/cursomc/domain/Cliente.java
+++ src/main/java/com/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente {
@@ -26,6 +27,7 @@ public class Cliente {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
---
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.repositories.ClienteRepository;
+import com.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
